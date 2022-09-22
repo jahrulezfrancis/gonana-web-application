f97ec5ad335559a50a0d5c1b42a832092d161548
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-22 18:28:36 +0100

    added bottom nav

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,6 +1,9 @@
-import { Box, Divider, HStack, Icon, ListItem, Spacer, Stack, StackDivider, UnorderedList, useMediaQuery } from '@chakra-ui/react'
+import { Box, Divider, HStack, Icon, ListItem, Spacer, Stack, UnorderedList, useMediaQuery } from '@chakra-ui/react'
 import React from 'react'
 import { IoRocketOutline, IoAddCircleOutline } from "react-icons/io5";
+import { BsGrid, BsFillCircleFill } from "react-icons/bs"
+import { FiShoppingBag, FiMessageCircle } from "react-icons/fi"
+import { MdOutlineArticle } from "react-icons/md"
 import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
@@ -39,9 +42,14 @@ export default function Navigation() {
 
 export function BottomNav() {
     return (
-        <Box>
-            <HStack>
-                
+        <Box position='fixed' bottom='0' right='0' left='0'>
+                <Divider />
+            <HStack spacing='2em' justify='space-around' p='1em'>
+                <Icon boxSize='2em' as={BsGrid} />
+                <Icon boxSize='2em' as={FiShoppingBag} />
+                <Icon boxSize='2em' as={MdOutlineArticle} />
+                <Icon boxSize='2em' as={FiMessageCircle} />
+                <Icon boxSize='2em' as={BsFillCircleFill} />
             </HStack>
         </Box>
     )
