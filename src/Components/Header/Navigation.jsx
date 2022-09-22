7e352e25a6e41918d5bb70b29366ceb37c80baa9
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-22 16:42:14 +0100

    Initial Commit

---
+++ src/Components/Header/Navigation.jsx
@@ -0,0 +1,13 @@
+import { Box, ListItem, UnorderedList } from '@chakra-ui/react'
+import React from 'react'
+
+
+export default function Navigation() {
+    return (
+        <Box>
+            <UnorderedList>
+                <ListItem>we out here testing</ListItem>
+            </UnorderedList>
+        </Box>
+    )
+}
---
+++ src/Components/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import Navigation from '../Header/Navigation';
+
+export default function HomePage() {
+    return (
+        <Box>
+            <Navigation />
+        </Box>
+    )
+}
