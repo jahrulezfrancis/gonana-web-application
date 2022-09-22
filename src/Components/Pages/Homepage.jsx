5935dc18c76f10fdcde8fd4216ae8cc340d9020c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-22 21:45:13 +0100

    Added the story section

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,12 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Stack, VStack } from '@chakra-ui/react';
 import React from 'react';
 import Navigation, { BottomNav } from '../Header/Navigation';
+import { Stories } from '../Sections/Stories';
 
 export default function HomePage() {
     return (
-        <Box>
-            <Navigation />
-            <BottomNav />
-        </Box>
+        <VStack>
+            <Box>
+                <Navigation />
+                <Stack mt='3.5em' align='start' justify='flex-start'>
+                    <Stories />
+                </Stack>
+                <BottomNav />
+            </Box>
+        </VStack>
     )
 }
