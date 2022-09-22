020a01135beb86efa3a133712eb812854eb3a6d0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-22 17:29:57 +0100

    inserted bottom nav to hompage

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
-import Navigation from '../Header/Navigation';
+import Navigation, { BottomNav } from '../Header/Navigation';
 
 export default function HomePage() {
     return (
         <Box>
             <Navigation />
+            <BottomNav />
         </Box>
     )
 }
