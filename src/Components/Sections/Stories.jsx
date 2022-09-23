e90183a091e43dbdfacea18f3b5f7736a8c72b8e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-23 13:51:13 +0100

    changed the story display size

--- src/Components/Sections/Stories.jsx
+++ src/Components/Sections/Stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, HStack, Avatar, useMediaQuery } from '@chakra-ui/react';
+import { Box, HStack, Avatar, useMediaQuery, VStack, Stack } from '@chakra-ui/react';
 import User1 from "../Images/Jahrulez.jpg"
 import User2 from "../Images/Jahrulez2.png"
 import User3 from "../Images/woman.svg"
@@ -7,14 +7,24 @@ import User3 from "../Images/woman.svg"
 export function Stories() {
     const [onMobile] = useMediaQuery('(max-width: 1000px)')
     return (
-        <Box h='auto' w='100%'>
-            <HStack h='auto' mt={onMobile ? '2em' : "4em"} justify='center' w='100%' overflowX='auto' whiteSpace='nowrap'>
-                <Avatar size={onMobile ? 'lg' : '2xl'} name='Dan Abrahmov' src={User1} />
-                <Avatar size={onMobile ? 'lg' : '2xl'} name='Dan Abrahmov' src={User2} />
-                <Avatar size={onMobile ? 'lg' : '2xl'} name='Dan Abrahmov' src={User3} />
-                <Avatar size={onMobile ? 'lg' : '2xl'} name='Dan Abrahmov' src={User1} />
-                <Avatar size={onMobile ? 'lg' : '2xl'} name='Dan Abrahmov' src={User2} />
-            </HStack>
+        <Box h='200vh' w='100%' mt={onMobile ? '2em' : "4em"} overflowX='hidden'>
+            <Stack align='flex-start'>
+                <HStack justify='start' align='start' w='100%' overflowX='auto' whiteSpace='nowrap'>
+                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User1} />
+                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User2} />
+                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User3} />
+                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User1} />
+                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User2} />
+                </HStack>
+            </Stack>
         </Box>
+    )
+}
+
+export default function StoryPage() {
+    return (
+        <VStack justify='start' align='start'>
+            <Stories />
+        </VStack>
     )
 }
