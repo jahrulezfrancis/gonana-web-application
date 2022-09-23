89b77e6548b2811a599c1828c6db7b6fc8ae0b2e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-23 10:17:18 +0100

    Added route to orders page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Components/Pages/Homepage';
+import Orders from './Components/Pages/Orders';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='home' element={<HomePage />} />
+      <Route path='orders' element={<Orders />} />
     </Routes>
   );
 }
