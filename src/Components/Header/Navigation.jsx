1b25050e6d8bdfe526b60512ff18fd58774c6aa7
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-23 10:18:09 +0100

    created nav template

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -6,8 +6,9 @@ import { FiShoppingBag, FiMessageCircle } from "react-icons/fi"
 import { MdOutlineArticle } from "react-icons/md"
 import { NavLink } from 'react-router-dom';
 
-export default function Navigation() {
 
+
+export function NavigationTemplate(props) {
     const [MobileWidth] = useMediaQuery('(max-width: 1000px)')
 
     return (
@@ -16,19 +17,19 @@ export default function Navigation() {
                 <HStack>
                     <Stack align='center'>
                         <ListItem color='green.500' fontFamily='Open Sans' fontSize='2em' fontWeight='900'>
-                            <NavLink>GONANA</NavLink>
+                            <NavLink>{props.NavHeader}</NavLink>
                         </ListItem>
                     </Stack>
                     <Spacer />
                     <HStack align='center' pr={MobileWidth ? '0em' : '1em'} spacing='2em'>
                         <ListItem>
                             <NavLink>
-                                <Icon boxSize='2em' as={IoRocketOutline} />
+                                {props.icon1}
                             </NavLink>
                         </ListItem>
                         <ListItem>
                             <NavLink>
-                                <Icon boxSize='2em' as={IoAddCircleOutline} />
+                                {props.icon2}
                             </NavLink>
                         </ListItem>
                     </HStack>
@@ -40,16 +41,34 @@ export default function Navigation() {
 }
 
 
+export default function Navigation() {
+    return (
+        <NavigationTemplate NavHeader='Gonana' icon1={<IoRocketOutline size='2em' />} icon2={<IoAddCircleOutline size='2em' />} />
+    )
+
+}
+
+
 export function BottomNav() {
     return (
         <Box position='fixed' bottom='0' right='0' left='0'>
-                <Divider />
+            <Divider />
             <HStack spacing='2em' justify='space-around' p='1.8em'>
-                <Icon boxSize='2.5em' as={BsGrid} />
-                <Icon boxSize='2.5em' as={FiShoppingBag} />
-                <Icon boxSize='2.5em' as={MdOutlineArticle} />
-                <Icon boxSize='2.5em' as={FiMessageCircle} />
-                <Icon boxSize='2.5em' as={BsFillCircleFill} />
+                <NavLink to='/home'>
+                    <Icon boxSize='2.5em' as={BsGrid} />
+                </NavLink>
+                <NavLink to='/orders'>
+                    <Icon boxSize='2.5em' as={FiShoppingBag} />
+                </NavLink>
+                <NavLink >
+                    <Icon boxSize='2.5em' as={MdOutlineArticle} />
+                </NavLink>
+                <NavLink>
+                    <Icon boxSize='2.5em' as={FiMessageCircle} />
+                </NavLink>
+                <NavLink>
+                    <Icon boxSize='2.5em' as={BsFillCircleFill} />
+                </NavLink>
             </HStack>
         </Box>
     )
