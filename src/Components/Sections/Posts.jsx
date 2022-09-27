6f1c2d60b55b31933bc4cb5fef62ece8d004aba3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-27 20:50:43 +0100

    added post content from an array

---
+++ src/Components/Sections/Posts.jsx
@@ -0,0 +1,98 @@
+import { Box, Heading, Text, Image, Badge, HStack, Avatar, VStack } from "@chakra-ui/react";
+import { BsStars } from "react-icons/bs";
+
+import { Post } from "../DataBase/Post";
+
+
+
+function CardTemplate(props) {
+    const property = {
+        imageUrl: 'https://bit.ly/2Z4KKcF',
+        imageAlt: 'Rear view of modern home with pool',
+        
+        beds: 3,
+        baths: 2,
+        title: 'Modern home in city center in the heart of historic Los Angeles',
+        formattedPrice: '$1,900.00',
+        reviewCount: 34,
+        rating: 4,
+    }
+
+    return (
+        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+            <HStack p='1em'>
+                <Avatar name={props.userName} src="" />
+                <VStack spacing='-.1em'>
+                    <Text>{props.userName}</Text>
+                    <Text>{new Date().toLocaleDateString()}</Text>
+                </VStack>
+            </HStack>
+            <Box
+                mt='1'
+                fontWeight='semibold'
+                as='h4'
+                lineHeight='tight'
+                noOfLines={2}
+            >
+                {props.body}
+            </Box>
+            <Image src={property.imageUrl} alt={property.imageAlt} />
+
+            <Box p='6'>
+                <Box display='flex' alignItems='baseline'>
+                    <Badge borderRadius='full' px='2' colorScheme='teal'>
+                        New
+                    </Badge>
+                    <Box
+                        color='gray.500'
+                        fontWeight='semibold'
+                        letterSpacing='wide'
+                        fontSize='xs'
+                        textTransform='uppercase'
+                        ml='2'
+                    >
+                        {property.beds} beds &bull; {property.baths} baths
+                    </Box>
+                </Box>
+
+
+                <Box>
+                    {property.formattedPrice}
+                    <Box as='span' color='gray.600' fontSize='sm'>
+                        / wk
+                    </Box>
+                </Box>
+
+                <Box display='flex' mt='2' alignItems='center'>
+                    {Array(5)
+                        .fill('')
+                        .map((_, i) => (
+                            <BsStars
+                                key={i}
+                                color={i < property.rating ? 'teal.500' : 'gray.300'}
+                            />
+                        ))}
+                    <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                        {property.reviewCount} reviews
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+
+
+export default function PostCard() {
+  
+    return (
+        <Box>
+            {
+                Post.map((element) => {
+                    return <CardTemplate userName='Jahrulez' title={element.title} body={element.body} key={element.id} />
+                })
+            }
+        </Box>
+    )
+}
+
