f657c8a7d0eac152b6b3c1720a91c42663fff339
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-02 14:57:17 +0100

    added more stories

--- src/Components/Sections/Stories.jsx
+++ src/Components/Sections/Stories.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Box, HStack, Avatar, useMediaQuery, VStack, Stack } from '@chakra-ui/react';
-import User1 from "../Images/Jahrulez.jpg"
-import User2 from "../Images/Jahrulez2.png"
-import User3 from "../Images/woman.svg"
+import { Box, HStack, Avatar, useMediaQuery, VStack } from '@chakra-ui/react';
+import { UsersDB } from '../DataBase/Users';
 
-export function Stories() {
+
+
+export function StoryTemp(props) {
     const [onMobile] = useMediaQuery('(max-width: 1000px)')
     return (
-        <Box h='200vh' w='100%' mt={onMobile ? '2em' : "4em"} overflowX='hidden'>
-            <Stack align='flex-start'>
-                <HStack justify='start' align='start' w='100%' overflowX='auto' whiteSpace='nowrap'>
-                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User1} />
-                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User2} />
-                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User3} />
-                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User1} />
-                    <Avatar size={onMobile ? 'md' : 'md'} name='Dan Abrahmov' src={User2} />
-                </HStack>
-            </Stack>
+        <Box h='200vh' w='100%' mt={onMobile ? '2em' : "3.5em"} alignItems='center'>
+            <Avatar size={onMobile ? 'md' : 'md'} name={props.name} src={props.Avatar} />
         </Box>
+    )
+}
+
+export function Stories() {
+    return (
+        <HStack>
+            {UsersDB.map((element) => {
+                return (
+                    <Box key={element.id}>
+                        <StoryTemp name={element.username} Avatar={element.image} />
+                    </Box>
+                )
+            })}
+        </HStack>
     )
 }
 
 export default function StoryPage() {
     return (
-        <VStack justify='start' align='start' 
-        position='fixed' zIndex='2' bgColor='white'
-        top='10' right='0' left='0' bottom='75%' 
+        <VStack justify='start' align='start'
+            position='fixed' zIndex='2' bgColor='white'
+            top='10' right='0' left='0' bottom='75%'
         >
             <Stories />
         </VStack>
