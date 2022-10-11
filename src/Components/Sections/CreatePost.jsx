5e0eafe04f6cbde8e1f2e88b0e1aebd409cd891d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-11 22:26:37 +0100

    updated create post component

--- src/Components/Sections/CreatePost.jsx
+++ src/Components/Sections/CreatePost.jsx
@@ -1,9 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { IconButton, Icon, Input, useDisclosure } from "@chakra-ui/react";
+import { Modal, Button, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, } from '@chakra-ui/react'
+import { IoRocketOutline } from "react-icons/io5";
 
-export function CreatePost(){
-    return(
-        <Box>
-            
-        </Box>
+
+export function CreatePost() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return (
+        <>
+        <IconButton>
+
+            <Icon as={IoRocketOutline} onClick={onOpen} />
+        </IconButton>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create New Post</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Input type='text' />
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Publish
+                        </Button>
+                        <Button color='red' variant='ghost' onClick={onClose}>Discard</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
