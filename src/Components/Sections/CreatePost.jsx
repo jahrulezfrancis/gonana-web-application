83ae043663ee3dbab6486802d1601d990911cfe0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-14 06:37:33 +0100

    changed button and updated ui

--- src/Components/Sections/CreatePost.jsx
+++ src/Components/Sections/CreatePost.jsx
@@ -1,6 +1,7 @@
 import { IconButton, Icon, Input, useDisclosure, Alert, AlertIcon, AlertTitle, AlertDescription, Textarea } from "@chakra-ui/react";
 import { Modal, Button, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, } from '@chakra-ui/react'
-import { IoRocketOutline } from "react-icons/io5";
+import { FcAddImage } from "react-icons/fc"
+import {MdOutlineAddCircleOutline} from "react-icons/md"
 
 
 
@@ -15,23 +16,19 @@ function DeleteWarning() {
                 <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
             </Alert>
             <Button onClick={onOpen}>Alert Check</Button>
-            
+
         </>
     )
 }
 
-function FilePicker(){
-    
-        <Input placeholder="Choose file" type='file' />
-}
 
 export function CreatePost() {
     const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
         <>
-            <IconButton onClick={onOpen} >
-                <Icon as={IoRocketOutline} />
+            <IconButton bg='transparent' onClick={onOpen} >
+                <Icon boxSize='1.5em' as={MdOutlineAddCircleOutline} />
             </IconButton>
 
             <Modal isOpen={isOpen} onClose={onClose}>
@@ -40,8 +37,11 @@ export function CreatePost() {
                     <ModalHeader>Create New Post</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <Textarea placeholder='Post Body' />
-                       <Button onClick={() => { return  <Input placeholder="Choose file" type='file' /> }}>Select File</Button>
+                        <Textarea placeholder="What's on your mind" />
+                        <Input display='none' placeholder="Choose file" type='file' accept="image/*" id="image-picker" />
+                        <label style={{ padding: '.4em' }} for='image-picker' >
+                            <Icon fontSize='3em' as={FcAddImage} />
+                        </label>
                     </ModalBody>
 
                     <ModalFooter>
