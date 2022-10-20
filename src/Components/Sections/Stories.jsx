dba53f6553edb465235cc8a2157d73c8067a5924
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-20 20:58:35 +0100

    updated story component

--- src/Components/Sections/Stories.jsx
+++ src/Components/Sections/Stories.jsx
@@ -14,7 +14,7 @@ function StoryTemp(props) {
 export function Stories() {
     const [onMobile] = useMediaQuery('(max-width: 1000px)')
     return (
-        <HStack position='fixed' w='100vw' overflowX='auto' mt={onMobile ? '.5em' : '2.2em'} bg='#ffffff' p='1em' zIndex='2'>
+        <HStack position='fixed' w='100vw' overflowX='auto' mt={onMobile ? '.5em' : '1.2em'} bg='#ffffff' p='1em' zIndex='2'>
             {StoryDB.map((element) => {
                 return (
                     <Box key={element.id}>
